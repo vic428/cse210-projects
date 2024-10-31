059b033f724a82dc16a0a88ea9c80d1a7da491a9
vic428 <varchibong428@gmail.com>
2024-10-31 13:50:37 -0600

    Made changes to the dates on the instances

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         // Creating instances of each activity
-        Activity running = new Running(new DateTime(2022, 11, 3), 30, 4.8); // Distance in km
-        Activity cycling = new Cycling(new DateTime(2022, 11, 3), 30, 20); // Speed in kph
-        Activity swimming = new Swimming(new DateTime(2022, 11, 3), 30, 20); // Laps
+        Activity running = new Running(new DateTime(2024, 10, 31), 30, 4.8); // Distance in km
+        Activity cycling = new Cycling(new DateTime(2024, 10, 31), 30, 20); // Speed in kph
+        Activity swimming = new Swimming(new DateTime(2024, 10, 31), 30, 20); // Laps
 
         // Adding activities to a list
         List<Activity> activities = new List<Activity> { running, cycling, swimming };
